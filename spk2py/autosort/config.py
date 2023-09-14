f6ecdef1e7076108a738c9f40f9c1123abba3ece
Flynn OConnell <flynnoconnell@gmail.com>
2023-09-14 17:05:53 -0400

    fix configs and paths

--- spk2py/autosort/config.py
+++ spk2py/autosort/config.py
@@ -1,100 +1,151 @@
-import os
+from __future__ import annotations
 import configparser
 from pathlib import Path
 
-config_ver = 5
 
-
-def do_the_config(path=''):
-    path = Path().home() / 'autosort' / 'autosort_config.ini' if not path else Path(path)
-    path = Path(os.path.expanduser('~')) / 'autosort' / 'autosort_config.ini'
+def set_config(path: Path | str = '', default=False) -> dict[str, str | int | float]:
+    if default:
+        path = Path().home() / 'spk2py' / 'autosort' / 'default_config.ini'
 
     if not path.is_file():
         default_config(path)
         print(f'Default configuration file has been created. You can find it in {path}')
+        return read_config(path)
     else:
         return read_config(path)
 
 
-def default_config(path):
+def default_config(path: Path, config_ver: int = 5) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
+    default_data_path = Path().home() / 'data'
+    default_run_path = default_data_path / 'to_run'
+    default_results_path = default_data_path / 'results'
+    default_completed_path = default_data_path / 'completed'
 
     config = configparser.ConfigParser()
-    config['Run Settings'] = {
-        'Resort Limit': '3',
-        'Cores Used': '8',
-        'Weekday Run': '2',
-        'Weekend Run': '8',
-        'Run Type': 'Auto',
-        'Manual Run': '2'
+    config['run-settings'] = {
+        'resort-limit': '3',
+        'cores-used': '8',
+        'weekday-run': '2',
+        'weekend-run': '8',
+        'run-type': 'Auto',
+        'manual-run': '2'
     }
-    config['Paths'] = {
-        'to-run-path': '',
-        'running-path': '',
-        'results-path': '',
-        'completed-path': '',
-        'use-path': '1',
-        'Else Path': ''
+    config['paths'] = {
+        'run-path': str(default_run_path),
+        'results-path': str(default_results_path),
+        'completed-path': str(default_completed_path),
     }
-    config['Clustering'] = {
-        'Max Clusters': '7',
-        'Max Iterations': '1000',
-        'Convergence Criterion': '.0001',
-        'Random Restarts': '10',
-        'L-ratio Cutoff': '.1'
+    config['clustering'] = {
+        'max-clusters': '7',
+        'max-iterations': '1000',
+        'convergence-criterion': '.0001',
+        'random-restarts': '10',
+        'l-ratio-cutoff': '.1'
     }
-    config['Signal'] = {
-        'Disconnect Voltage': '1500',
-        'Max Breach Rate': '.2',
-        'Max Breach Count': '10',
-        'Max Breach Avg.': '20',
-        'Intra-Cluster Cutoff': '3'
+    config['signal'] = {
+        'disconnect-voltage': '1500',
+        'max-breach-rate': '.2',
+        'max-breach-count': '10',
+        'max-breach-avg': '20',
+        'intra-cluster-cutoff': '3'
     }
-    config['Filtering'] = {
-        'Low Cutoff': '600',
-        'High Cutoff': '3000'
+    config['filtering'] = {
+        'low-cutoff': '600',
+        'high-cutoff': '3000'
     }
-    config['Spike'] = {
-        'Pre-time': '.2',
-        'Post-time': '.6',
-        'Sampling Rate': '20000'
+    config['spike'] = {
+        'pre-time': '.2',
+        'post-time': '.6',
+        'sampling-rate': '20000'
     }
-    config['Std Dev'] = {
-        'Spike Detection': '2.0',
-        'Artifact Removal': '10.0'
+    config['std-dev'] = {
+        'spike-detection': '2.0',
+        'artifact-removal': '10.0'
     }
-    config['PCA'] = {
-        'Variance Explained': '.95',
-        'Use Percent Variance': '1',
-        'Principal Component n': '5'
+    config['pca'] = {
+        'variance-explained': '.95',
+        'use-percent-variance': '1',
+        'principal-component-n': '5'
     }
-    config['Post Process'] = {
+    config['post-process'] = {
         'reanalyze': '0',
-        'simple gmm': '1',
-        'image size': '70',
-        'temporary dir': Path().home() / 'tmp_python'
+        'simple-gmm': '1',
+        'image-size': '70',
+        'temporary-dir': str(Path.home() / 'tmp_python')
     }
-    config['Version'] = {
-        'config version': str(config_ver)
+    config['version'] = {
+        'config-version': str(config_ver)
     }
 
     with open(path, 'w') as configfile:
         config.write(configfile)
 
 
-def read_config(path):
+def read_config(path: Path, config_ver: int = 5):
     config = configparser.ConfigParser()
-    params = {}
     config.read(path)
-    for key, value in config._sections.items():
-        params.update(value)
 
-    if config_ver != int(params['config version']):
-        path.rename(path.with_suffix(str(params['config version']) + '.txt'))
-        default_config(path)
+    if 'config-version' not in config['version'] or int(config['version']['config-version']) != config_ver:
+        new_path = path.with_suffix(f'.{config["version"].get("config-version", "unknown")}.txt')
+        path.rename(new_path)
+        default_config(path, config_ver)
         print(
             f'Config version updated, config file reset to default, your original config file has been renamed.'
             f' Find the new config file here: {path}'
         )
-    else:
-        return params
+        config = configparser.ConfigParser()
+        config.read(path)
+
+    return get_config_params(config)
+
+
+def get_config_params(config: configparser.ConfigParser) -> dict[str, str | int | float]:
+    return {
+        "resort_limit": config.getint('run-settings', 'resort-limit'),
+        "cores_used": config.getint('run-settings', 'cores-used'),
+        "weekday_run": config.getint('run-settings', 'weekday-run'),
+        "weekend_run": config.getint('run-settings', 'weekend-run'),
+        "run_type": config.get('run-settings', 'run-type'),
+        "manual_run": config.getint('run-settings', 'manual-run'),
+
+        "to_run_path": config.get('paths', 'to-run-path'),
+        "running_path": config.get('paths', 'running-path'),
+        "results_path": config.get('paths', 'results-path'),
+        "completed_path": config.get('paths', 'completed-path'),
+        "use_path": config.getint('paths', 'use-path'),
+        "else_path": config.get('paths', 'else-path'),
+
+        "max_clusters": config.getint('clustering', 'max-clusters'),
+        "max_iterations": config.getint('clustering', 'max-iterations'),
+        "convergence_criterion": config.getfloat('clustering', 'convergence-criterion'),
+        "random_restarts": config.getint('clustering', 'random-restarts'),
+        "l_ratio_cutoff": config.getfloat('clustering', 'l-ratio-cutoff'),
+
+        "disconnect_voltage": config.getint('signal', 'disconnect-voltage'),
+        "max_breach_rate": config.getfloat('signal', 'max-breach-rate'),
+        "max_breach_count": config.getint('signal', 'max-breach-count'),
+        "max_breach_avg": config.getint('signal', 'max-breach-avg'),
+        "intra_cluster_cutoff": config.getint('signal', 'intra-cluster-cutoff'),
+
+        "low_cutoff": config.getint('filtering', 'low-cutoff'),
+        "high_cutoff": config.getint('filtering', 'high-cutoff'),
+
+        "pre_time": config.getfloat('spike', 'pre-time'),
+        "post_time": config.getfloat('spike', 'post-time'),
+        "sampling_rate": config.getint('spike', 'sampling-rate'),
+
+        "spike_detection": config.getfloat('std-dev', 'spike-detection'),
+        "artifact_removal": config.getfloat('std-dev', 'artifact-removal'),
+
+        "variance_explained": config.getfloat('pca', 'variance-explained'),
+        "use_percent_variance": config.getint('pca', 'use-percent-variance'),
+        "principal_component_n": config.getint('pca', 'principal-component-n'),
+
+        "reanalyze": config.getint('post-process', 'reanalyze'),
+        "simple_gmm": config.getint('post-process', 'simple-gmm'),
+        "image_size": config.getint('post-process', 'image-size'),
+        "temporary_dir": config.get('post-process', 'temporary-dir'),
+
+        "config_version": config.get('version', 'config-version'),
+    }
