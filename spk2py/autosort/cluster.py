b8ff62ade8a5a450d362fecd3d0a43cbaa29b1af
Flynn OConnell <FlynnOConnell@gmail.com>
2023-09-19 20:17:32 -0400

    make sampling rate non-optional, add jit compilation

--- spk2py/autosort/cluster.py
+++ spk2py/autosort/cluster.py
@@ -10,6 +10,7 @@ from sklearn.mixture import GaussianMixture
 from .utils import excepts
 import logging
 from pathlib import Path
+from numba import jit
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -48,21 +49,22 @@ def filter_signal(sig, freq=(300, 6000), sampling_rate=20000):
     return filt_el
 
 
+@jit
 def extract_waveforms(
-    filt_el, spike_snapshot=None, sampling_rate=20000.0, STD=2.0, cutoff_std=10.0
+    signal: np.ndarray, sampling_rate: int, spike_snapshot: tuple=(0.2, 0.6), STD=2.0, cutoff_std=10.0
 ):
     """
     Extract individual spike waveforms from the filtered electrode signal.
 
     Parameters
     ----------
-    filt_el : array-like
+    signal : array-like
         The filtered electrode signal as a 1-D array.
-    spike_snapshot : list of float, optional
+    sampling_rate : float
+        The sampling rate of the signal in Hz.
+    spike_snapshot : tuple of float, optional
         The time range (in milliseconds) around each spike to extract, given as [pre_spike_time, post_spike_time].
-        Default is [0.2, 0.6].
-    sampling_rate : float, optional
-        The sampling rate of the signal in Hz. Default is 20000.0 Hz.
+        Default is (0.2, 0.6).
     STD : float, optional
         The number of standard deviations to use for the spike detection threshold. Default is 2.0.
     cutoff_std : float, optional
@@ -75,11 +77,9 @@ def extract_waveforms(
     spike_times : list of int
         List of indices indicating the positions of the extracted spikes in the input array.
     """
-    if spike_snapshot is None:
-        spike_snapshot = [0.2, 0.6]
-    m = np.mean(filt_el)
-    th = np.std(filt_el) * STD
-    pos = np.where(filt_el <= m - th)[0]
+    m = np.mean(signal)
+    th = np.std(signal) * STD
+    pos = np.where(signal <= m - th)[0]
     changes = []
     for i in range(len(pos) - 1):
         if pos[i + 1] - pos[i] > 1:
@@ -89,23 +89,23 @@ def extract_waveforms(
     spike_times = []
     for i in range(len(changes) - 1):
         minimum = np.where(
-            filt_el[pos[changes[i] : changes[i + 1]]]
-            == np.min(filt_el[pos[changes[i] : changes[i + 1]]])
+            signal[pos[changes[i]: changes[i + 1]]]
+            == np.min(signal[pos[changes[i]: changes[i + 1]]])
         )[0]
         if pos[minimum[0] + changes[i]] - int(
             (spike_snapshot[0] + 0.1) * (sampling_rate / 1000.0)
         ) > 0 and pos[minimum[0] + changes[i]] + int(
             (spike_snapshot[1] + 0.1) * (sampling_rate / 1000.0)
         ) < len(
-            filt_el
+            signal
         ):
-            tempslice = filt_el[
+            tempslice = signal[
                 pos[minimum[0] + changes[i]]
                 - int((spike_snapshot[0] + 0.1) * (sampling_rate / 1000.0)) : pos[
                     minimum[0] + changes[i]
                 ]
                 + int((spike_snapshot[1] + 0.1) * (sampling_rate / 1000.0))
-            ]
+                        ]
             if ~np.any(np.absolute(tempslice) > (th * cutoff_std) / STD):
                 slices.append(tempslice)
                 spike_times.append(pos[minimum[0] + changes[i]])
